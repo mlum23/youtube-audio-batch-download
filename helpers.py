027f69294795e80e7d4479f99b3d3b5a24ba0675
mlum23 <mlum23@my.bcit.ca>
2020-12-18 15:56:23 -0800

    Completed docstrings, removed update_dl_size

--- helpers.py
+++ helpers.py
@@ -1,3 +1,9 @@
+"""
+This module contains various helper functions for the
+YoutubeAudioBatchDownloader module.
+"""
+
+
 import io
 from PIL import Image, ImageTk
 import requests
@@ -5,7 +11,16 @@ import datetime
 from default_values import DownloadSize
 
 
-def get_img_data(img_url, maxsize=(500, 500), first=False):
+def get_img_data(img_url, first=False):
+    """
+    Returns the image data.
+
+    :param img_url: the URL of the image, as a string.
+    :param first: False if at least one event occured in the PySimpleGUI event loop.
+                  True otherwise.
+    :return: the image data, as an ImageTk.PhotoImage object
+    """
+    maxsize = (500, 500)
     response = requests.get(img_url, stream=True)
     img = Image.open(response.raw)
     img.thumbnail(maxsize)
@@ -17,35 +32,35 @@ def get_img_data(img_url, maxsize=(500, 500), first=False):
     return ImageTk.PhotoImage(img)
 
 
-def update_thumbnail_preview(window, video):
-    # Update title
-    video_title = window['-video_title-']
-    title = video.title
-    video_title.update(title)
-
-    # Update image
-    video_img = window['-video_preview-']
-    img_url = video.thumbnail_url
-    img_data = get_img_data(img_url)
-    video_img.update(data=img_data)
-
-
 def generate_folder():
+    """
+    Returns the name of the download folder with all the files.
+    :return: the name of the download folder with all the files, as a string.
+    """
     date_time = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
     name = f'Youtube_Audio_Batch_Downloader_{date_time}'
     return name
 
 
 def disable_buttons(is_disabled, *args):
+    """
+    Disables the button based on is_disabled.
+
+    :param is_disabled: a boolean, True/False
+    :param args: an array of PySimpleGui.Button's
+    """
     for button in args:
         button.update(disabled=is_disabled)
 
 
-def update_download_size_message(download_size, window):
-    window[DownloadSize.DOWNLOAD_SIZE].update(f'Approx. Download Size: {humansize(download_size)}')
-
-
 def humansize(nbytes):
+    """
+    Returns the converted bytes to a more readable number.
+
+    Source: https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
+    :param nbytes: the number of bytes, as an integer.
+    :return: the converted bytes, as a string.
+    """
     suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
     i = 0
     while nbytes >= 1024 and i < len(suffixes)-1:
@@ -53,4 +68,3 @@ def humansize(nbytes):
         i += 1
     f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
     return '%s %s' % (f, suffixes[i])
-
